bf689180d5c50f2ec48c256a5a228e8ef8c5dff6
Fahim Faisal <fahimfaisal1998@gmail.com>
2021-06-22 12:55:08 +0600

    M1F1

---
+++ resources/views/login.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login</title>
+</head>
+<body>
+    <center>
+          <form action="/login" method="POST">
+            <br>
+            <label for="uname">Username:</label>
+            <input type="text" name="uname" id="uname"><br><br>
+            <label for="password">Password:</label>
+            <input type="password" name="password" id="password"><br><br>
+            <input type="submit" value="Login">
+            </form>
+      </center>
+</body>
+</html>
